c5fd1563e84284877263d8227f1ac158e959a910
aphilthedoc <aphilthedoc@gmail.com>
2018-09-24 14:39:31 +0200

    correctif j-c

--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -11,23 +11,18 @@
  */
 function jadenCase(sentence){
 	let words=sentence.split(' ');
-	console.log(words);
  	for (let i=0; i<words.length; i++){
  		let eachWord=words[i].toLowerCase().split();
- 		console.log(eachWord);
  		for (let j=0; j<eachWord.length; j++){
  		let eachLetter=eachWord[j].split('');
- 		console.log(eachLetter);
  		eachLetter[0]=eachLetter[0].toUpperCase(0);
- 		console.log(eachLetter);
  		eachWord[j]=eachLetter.join('');
- 		console.log(eachWord[j]);
  		words[i]=eachWord[j];
  		}
- 	};join(' ');
+ 	}
+ 	sentence=words.join(' ');
 	return sentence.trimRight();
-}
- 	
+} 	
 console.log(jadenCase('You must write your own tests'));
 
 //* Begin of tests
